1ff80be5c0bd62dad8edd693ea61475864fb443d
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-10 16:17:37 +0700

    [Edited]: replace os.Lstat to os.Stat

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	var saveFileUnixTime int64
 	for {
-		f1, _ := os.Lstat("file.1")
+		f1, _ := os.Stat("file.1")
 		f1UnixTime := f1.ModTime().Unix()
 		if saveFileUnixTime != f1UnixTime {
 			saveFileUnixTime = f1UnixTime
