05a69cebbe33c5633ccaa1a229096f21d18162ed
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2018-11-10 17:37:13 +0700

    [Edited]: move print file with pattern to new function

--- main.go
+++ main.go
@@ -8,23 +8,32 @@ import (
 
 var ignoreFile = map[string]bool{}
 
-func WalkInDirectories(pathFile string) {
-	files, _ := ioutil.ReadDir(pathFile)
+func WalkInDirectories(previousPathFile, currentDirectory string, tabSize int) {
+	currentPathFile := path.Join(previousPathFile, currentDirectory)
+	files, _ := ioutil.ReadDir(currentPathFile)
 	for _, file := range files {
 		if ignoreFile[file.Name()] {
 			continue
 		}
+		printFileNameWithPrefix(file.Name(), tabSize)
 		if file.IsDir() {
-			fmt.Println(file.Name(), file.Size(), file.ModTime().Unix())
-			WalkInDirectories(path.Join(pathFile, file.Name()))
+			WalkInDirectories(currentPathFile, file.Name(), tabSize+1)
 		}
 	}
+}
+
+func printFileNameWithPrefix(filename string, tabSize int) {
+	for i := 0; i < tabSize; i++ {
+		fmt.Print("|  ")
+	}
+	fmt.Println("|-", filename)
 }
 
 func main() {
 	// ignore files
 	ignoreFile[".git"] = true
+	ignoreFile[".DS_Store"] = true
 
 	// start to walk directory from current path
-	WalkInDirectories(path.Dir(""))
+	WalkInDirectories(path.Dir("."), ".", 0)
 }
